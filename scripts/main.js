489db733d479c880151998f198dac9db473b4682
thupham <thupv.personal@gmail.com>
2020-09-04 21:24:44 +0700

    add drag drop, onClick event, highlight when hover

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@ const MESH_NAME = {
   GROUND: 'ground',
   BOX: 'box',
   DONUT: 'donut',
-  SPHERE: 'sphere'
+  SPHERE: 'sphere',
+  CURSOR: 'cursor'
 }
 
 const createBabylonEngine = (canvas) => {
@@ -23,6 +24,7 @@ const createBabylonScene = (engine) => {
 const createCamera = (scene, canvas) => {
   const camera = new BABYLON.UniversalCamera("Camera", new BABYLON.Vector3(20, 200, 400), scene);
   camera.setTarget(BABYLON.Vector3.Zero());
+  camera.invertRotation = true;
   camera.lowerBetaLimit = 0.1;
   camera.upperBetaLimit = (Math.PI / 2) * 0.99;
   camera.lowerRadiusLimit = 150;
@@ -41,7 +43,21 @@ const createGroundPlane = (scene) => {
   ground.material = groundMaterial;
 };
 
-const createSphere = (scene) => {
+const addOnClickEvent = (mesh, scene, onClickFnc = () => {
+}) => {
+  mesh.isPickable = true;
+  mesh.actionManager = new BABYLON.ActionManager(scene);
+  mesh.actionManager.registerAction(
+    new BABYLON.ExecuteCodeAction(
+      BABYLON.ActionManager.OnPickTrigger, () => {
+        onClickFnc.call(mesh, mesh);
+      }
+    )
+  )
+};
+
+const createSphere = (scene, onClick = () => {
+}) => {
   const sphere = BABYLON.Mesh.CreateSphere(MESH_NAME.SPHERE, 32, 20, scene);
   const redMat = new BABYLON.StandardMaterial(MESH_NAME.SPHERE + '_mat', scene);
   redMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
@@ -50,10 +66,12 @@ const createSphere = (scene) => {
   sphere.material = redMat;
   sphere.position.y = 10;
   sphere.position.x -= 100;
+  addOnClickEvent(sphere, scene, onClick);
   return sphere;
 };
 
-const createBox = (scene) => {
+const createBox = (scene, onClick = () => {
+}) => {
   const box = BABYLON.Mesh.CreateBox(MESH_NAME.BOX, 20, scene);
   const greenMat = new BABYLON.StandardMaterial(MESH_NAME.BOX + '_mat', scene);
   greenMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
@@ -62,10 +80,12 @@ const createBox = (scene) => {
   box.material = greenMat;
   box.position.z -= 100;
   box.position.y = 10;
+  addOnClickEvent(box, scene, onClick);
   return box;
 };
 
-const createDonut = (scene) => {
+const createDonut = (scene, onClick = () => {
+}) => {
   const donut = BABYLON.Mesh.CreateTorus(MESH_NAME.DONUT, 20, 2, 32, scene);
   const purpleMat = new BABYLON.StandardMaterial(MESH_NAME.DONUT + '_mat', scene);
   purpleMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
@@ -74,10 +94,15 @@ const createDonut = (scene) => {
   donut.material = purpleMat;
   donut.position.y = 10;
   donut.position.z += 100;
+  addOnClickEvent(donut, scene, onClick);
   return donut;
 };
 
-const addControl = (scene, canvas, camera) => {
+const createHighLightLayer = (scene) => {
+  return new BABYLON.HighlightLayer("highlightLayer", scene);
+}
+
+const addControl = (scene, canvas, camera, highLightLayer) => {
   const animateObjectToPosition = function (object, speed, frameCount, newPos) {
     camera.detachControl(canvas, false);
     const ease = new BABYLON.CubicEase();
@@ -91,8 +116,8 @@ const addControl = (scene, canvas, camera) => {
   }
 
   const createCursorOnGround = (scene) => {
-    const cursor = BABYLON.Mesh.CreateTorus(MESH_NAME.DONUT, 30, 10, 32, scene);
-    const yellowMat = new BABYLON.StandardMaterial(MESH_NAME.DONUT + '_mat', scene);
+    const cursor = BABYLON.Mesh.CreateTorus(MESH_NAME.CURSOR, 30, 10, 32, scene);
+    const yellowMat = new BABYLON.StandardMaterial(MESH_NAME.CURSOR + '_mat', scene);
     yellowMat.diffuseColor = new BABYLON.Color3(0.4, 0.4, 0.4);
     yellowMat.specularColor = new BABYLON.Color3(0.4, 0.4, 0.4);
     yellowMat.emissiveColor = BABYLON.Color3.Yellow();
@@ -110,6 +135,18 @@ const addControl = (scene, canvas, camera) => {
 
     return null;
   }
+
+  const getObjectOnMouse = (scene) => {
+    const pickInfo = scene.pick(scene.pointerX, scene.pointerY, function (mesh) {
+      return mesh.name !== MESH_NAME.GROUND && mesh.name !== MESH_NAME.CURSOR
+    });
+    if (pickInfo.hit) {
+      return pickInfo.pickedMesh;
+    }
+
+    return null;
+  };
+
   const cursor = createCursorOnGround();
   const speed = 60;
   const frameCount = 200;
@@ -118,19 +155,28 @@ const addControl = (scene, canvas, camera) => {
     startPos = getPositionCursorOnGround(scene);
   }
   const onMouseMove = (e) => {
+    const meshOnHover = getObjectOnMouse(scene);
+    if (meshOnHover) {
+      cursor.setEnabled(false);
+      if (!highLightLayer.hasMesh(meshOnHover)) {
+        highLightLayer.addMesh(meshOnHover, BABYLON.Color3.Black());
+      }
+      return;
+    }
+    highLightLayer.removeAllMeshes();
+    cursor.setEnabled(true);
     const position = getPositionCursorOnGround(scene);
-    if(!position) return;
+    if (!position) return;
+
     cursor.position.x = position.x;
     cursor.position.y = position.y;
     cursor.position.z = position.z;
   }
   const onMouseUp = (e) => {
     const position = getPositionCursorOnGround(scene);
-    if(!position) return;
+    if (!position) return;
     const diff = startPos.subtract(position);
-    console.log(diff);
-    console.log(diff.length);
-    if(diff.length() > 1) return;
+    if (diff.length() > 1) return;
     const newPosition = new BABYLON.Vector3(position.x, camera.position.y, position.z);
     animateObjectToPosition(camera, speed, frameCount, newPosition);
   }
@@ -144,12 +190,16 @@ const main = () => {
   const engine = createBabylonEngine(canvas);
   const scene = createBabylonScene(engine);
   const camera = createCamera(scene, canvas);
+  const highlightLayer = createHighLightLayer(scene);
+  const listObjects = [];
   createLight(scene);
   createGroundPlane(scene);
-  createBox(scene);
-  createDonut(scene);
-  createSphere(scene);
-  addControl(scene, canvas, camera);
+  listObjects.push(createBox(scene, (box) => {
+    box.setEnabled(false);
+  }));
+  listObjects.push(createDonut(scene));
+  listObjects.push(createSphere(scene));
+  addControl(scene, canvas, camera, highlightLayer);
   engine.runRenderLoop(() => {
     scene.render();
   });
